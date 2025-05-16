d175689a54bb150a1d89b11c4f20d5c88957f047
isavb03 <jocvb13@gmail.com>
2025-05-16 14:05:05 +0200

    Files created by spring initializr, initial config

---
+++ src/main/java/com/servicio/admin/admin_service/ServicioDeAdministradoresApplication.java
@@ -0,0 +1,13 @@
+package com.servicio.admin.admin_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioDeAdministradoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioDeAdministradoresApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/servicio/admin/admin_service/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.servicio.admin.admin_service;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ServicioDeAdministradoresApplication.class);
+	}
+
+}
