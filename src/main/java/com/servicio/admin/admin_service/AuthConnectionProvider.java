e71866f858f106a274326dedeff5d41f4ce855c0
isavb03 <jocvb13@gmail.com>
2025-05-19 20:50:44 +0200

    ayuda

--- src/main/java/com/servicio/admin/admin_service/AuthConnectionProvider.java
+++ src/main/java/com/servicio/admin/admin_service/AuthConnectionProvider.java
@@ -1,6 +1,7 @@
 package com.servicio.admin.admin_service;
 
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.DriverManager;
@@ -8,11 +9,16 @@ import java.sql.DriverManager;
 @Component
 public class AuthConnectionProvider {
     
+    @Value("${spring.datasource.url}")
+    private String url;
+    
+    @Value("${spring.datasource.username}")
+    private String username;
+    
+    @Value("${spring.datasource.password}")
+    private String password;
+    
     public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/university",
-            "root",
-            "1234"
-        );
+        return DriverManager.getConnection(url, username, password);
     }
 }
--- src/main/java/com/servicio/admin/admin_service/AuthController.java
+++ src/main/java/com/servicio/admin/admin_service/AuthController.java
@@ -5,12 +5,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus;  // Added import for HttpStatus
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "*") // Important for cross-origin requests
 public class AuthController {
-
+    
+    @Value("${admin.username:admin}")
+    private String adminUsername;
+    
+    @Value("${admin.password:1234}")
+    private String adminPassword;
+    
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestParam String username, 
                                   @RequestParam String password) {
